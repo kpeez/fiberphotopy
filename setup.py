6e62a6ed1949eb3f81015589156756266d834d33
kpuhger <krpuhger@gmail.com>
2021-12-30 22:43:02 -0800

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fiberphotopy",
-    version="0.0.0",
+    version="0.0.1",
     description="Package for analyzing fiber photometry data",
     author="Kyle P",
     author_email="krpuhger@gmail.com",
