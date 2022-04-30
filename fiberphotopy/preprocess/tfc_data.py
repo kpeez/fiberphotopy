52b191c7cce1d415fe5ce9477555fb39c28e5d72
kpuhger <krpuhger@gmail.com>
2022-04-30 14:38:08 -0700

    updated tfc_trials_df docstring

--- fiberphotopy/preprocess/tfc_data.py
+++ fiberphotopy/preprocess/tfc_data.py
@@ -163,9 +163,10 @@ def get_tfc_trial_data(
 
 @save_data
 def tfc_trials_df(
-    df_session,
+    session_df,
     session="train",
     yvar="465nm_dFF",
+    trial_dff=False,
     trial_debleach=False,
     trial_start=-20,
     cs_dur=20,
@@ -174,29 +175,31 @@ def tfc_trials_df(
     iti_dur=120,
 ):
     """
-    1. Creates a dataframe of "Trial data", from (trial_start, trial_end) around each CS onset
-    2. Normalizes dFF for each trial to the avg dFF of each trial's pre-CS period.
-
-    ! Note: Session must be a sheet name in 'TFC phase components.xlsx'
+    1. Creates a dataframe of "Trial data", from (trial_start, trial_end) around each cue onset
+    2. Normalizes dFF for each trial to the avg dFF of each trial's baseline period (pre-cue).
 
     Args:
         df (DataFrame): Session data to be converted to trial-level format.
         session (str): Name of session used to label DataFrame. Defaults to "train".
         yvar (str): Name of dependent variable to trial-normalize. Defaults to "465nm_dFF".
-        normalize (bool, optional): Normalize yvar to baseline of each trial. Defaults to True.
-        trial_start (int, optional): Time at start of trial. Defaults to -20.
-        cs_dur (int, optional): CS duration used to calculate trial time. Defaults to 20.
-        trace_dur (int, optional): Duration of trace interval. Defaults to 20.
-            Set to 0 for delay conditioning.
-        us_dur (int, optional): Duration of unconditional stimulus. Defaults to 2.
-        iti_dur (int, optional): Duration of intertrial interval. Defaults to 120.
+        trial_dff (bool, optional): Fit trial_dFF on each trial. Defaults to False.
+        trial_debleach (bool, optional): Fit  biexponential on each trial to debleach. Defaults to False.
+        trial_start (int): _description_. Defaults to -20.
+        cs_dur (int): CS duration used to calculate trial time. Defaults to 20.
+        trace_dur (int): Duration of trace interval. Defaults to 20. Set to 0 for delay conditioning.
+        us_dur (int): Duration of unconditional stimulus. Defaults to 2.
+        iti_dur (int): Duration of intertrial interval. Defaults to 120.
 
     Returns:
         DataFrame: Trial-level data with `yvar` trial-normalized.
+
+
+    Notes:
+    - To use trial-level debleaching, you need to also set trial_dff = True.
     """
     df = get_tfc_trial_data(
-        df_session,
-        session="train",
+        session_df,
+        session=session,
         trial_start=trial_start,
         cs_dur=cs_dur,
         trace_dur=trace_dur,
@@ -206,19 +209,31 @@ def tfc_trials_df(
 
     df_list = []
     for animal in df["Animal"].unique():
-        if trial_debleach:
-            df_animal = debleach_signals(df.query("Animal == @animal"), by_trial=True)
+        if trial_dff:
+            df_animal = df.query("Animal == @animal").copy()
+            debleach_bool = False
+            _Y_ref = "405nm"
+            _Y_sig = "465nm"
+            yvar = "465nm_dFF"
+
+            if trial_debleach:
+                df_animal = debleach_signals(df_animal, by_trial=True)
+                debleach_bool = True
+                _Y_ref = "ref_debleach"
+                _Y_sig = "sig_debleach"
+                yvar = "sig_debleach_dFF"
+
             df_animal = fit_linear(
                 df_animal,
-                Y_ref="ref_debleach",
-                Y_sig="sig_debleach",
+                Y_ref=_Y_ref,
+                Y_sig=_Y_sig,
                 by_trial=True,
-                debleached=True,
+                debleached=debleach_bool,
             )
-            yvar = "sig_debleach_dFF"
+
         else:
             df_animal = df.query("Animal == @animal").copy()
 
-        df_list.append(trial_normalize(df_animal, yvar=yvar))
+        df_list.append(trial_normalize(df_animal, yvar))
 
     return pd.concat(df_list)
